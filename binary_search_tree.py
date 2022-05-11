a386ce35871e2da7d07b2cc416e91fd3e57b6e8a
vlcp197 <vlcp197@gmail.com>
2022-05-11 10:48:23 -0300

    Adicionados métodos de fila e pilha

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -14,7 +14,7 @@ class BinarySearchTree:
                 node.left = Node(data)
             else:
                 self._insert_recursive(data, node.left)
-        elif data > node.data:
+        elif data["id"] > node.data["id"]:
             if node.right is None:
                 node.right = Node(data)
             else:
@@ -29,21 +29,21 @@ class BinarySearchTree:
             self._insert_recursive(data,self.root)
         
     def _search_recursive(self, blog_post_id, node):
-        if node.left == None and node.right == None:
-            return False
-    
+        
         if blog_post_id == node.data["id"]:
             return node.data
         
-        if blog_post_id < node.data["id"]:
+        if blog_post_id < node.data["id"] and node.left is not None:
             if blog_post_id == node.left.data["id"]:
                 return node.left.data
-        return self._search_recursive(blog_post_id, node.left)
+            return self._search_recursive(blog_post_id, node.left)
         
-        if blog_post_id > node.data["id"]:
+        if blog_post_id > node.data["id"] and node.right is not None:
             if blog_post_id == node.right.data["id"]:
                 return node.right.data
             return self._search_recursive(blog_post_id, node.right)
+        
+        return False
         
     def search(self, blog_post_id):
         blog_post_id = int(blog_post_id)
